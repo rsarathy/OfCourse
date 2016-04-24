85c937bdb9b6adcb8560b2bd24d7ae2301ba6eed
Rohit Sarathy <rohit@sarathy.org>
2016-04-24 07:37:23 -0500

    cleanup

--- CoursePlanner/models.py
+++ CoursePlanner/models.py
@@ -14,14 +14,6 @@ class Course(models.Model):
     def __str__(self):
         return self.identifier
 
-    def __cmp__(self, other):
-        this_ident = str(self.identifier)
-        othr_ident = str(other.identifier)
-        return this_ident < othr_ident
-
-    def get_ident(self):
-        return self.identifier
-
     def get_credit(self):
         try:
             return int(self.credit[8])
