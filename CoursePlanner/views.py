235f2d5be7685fc45e96c21125da6a58dc1e3e8f
Rohit Sarathy <rohit@sarathy.org>
2016-04-24 07:22:02 -0500

    fixed validation bugs

--- CoursePlanner/views.py
+++ CoursePlanner/views.py
@@ -4,8 +4,6 @@ from django.shortcuts import render
 from CoursePlanner.forms import *
 from CoursePlanner.models import Course
 
-# all_courses = Catalog("courses.txt").get_courses()
-
 d = [[],[],[],[],[],[],[],[]]
 cr = [0, 0, 0, 0, 0, 0, 0, 0] # 8 semesters of credit
 urls = {}
@@ -18,7 +16,8 @@ def add_course(request):
     errors = []
     c = ""
     i = int(request.path[-2]) - 1
-    if request.method == 'POST':
+    print request.POST
+    if request.method == 'POST' and "Remove" not in request.POST.values():
         form = CourseForm(request.POST)
         if 'save_semester' in request.POST:
             return save_semester(request)
@@ -35,15 +34,15 @@ def add_course(request):
                     d[i].append(query[0])
                     d[i].sort(key=lambda x: x.identifier)
                     cr[i] += query[0].get_credit()
-        else:   #removal
-            cin = sorted(request.POST)
-            to_remove = str(cin[1][7:])
-            for c in d[i]:
-                if c.identifier == to_remove:
-                    d[i].remove(c)
-                    cr[i] -= c.get_credit()
-                    break
-            form = CourseForm()
+    elif request.method == 'POST' and "Remove" in request.POST.values():   #removal
+        cin = sorted(request.POST)
+        to_remove = str(cin[1][7:])
+        for c in d[i]:
+            if c.identifier == to_remove:
+                d[i].remove(c)
+                cr[i] -= c.get_credit()
+                break
+        form = CourseForm()
     else:
         form = CourseForm()
     return render(request, "selection.html",
@@ -54,6 +53,18 @@ def add_course(request):
         "credit": cr[i],
         "sem_number": i+1,
         })
+
+def save_semester(request):
+    return render(request, "semesters.html",
+        {
+        "urls": urls,
+        "courses": d,
+        "credit": cr,
+        "cr_hours": sum(cr),
+        })
+
+def plans(request):
+    pass
 
 def login(request):
     userid = ""
@@ -105,15 +116,3 @@ def signup(request):
         "form": form,
         "errors": errors,
         })
-
-def save_semester(request):
-    return render(request, "semesters.html",
-        {
-        "urls": urls,
-        "courses": d,
-        "credit": cr,
-        "cr_hours": sum(cr),
-        })
-
-def plans(request):
-    pass
